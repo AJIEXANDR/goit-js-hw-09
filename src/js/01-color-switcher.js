39055c56f3dcf89772996b093f9c041cf7468978
Aleksandr Tkachenko <saylass@ukr.net>
2023-03-31 21:54:21 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,30 @@
 const refs = {
-  startButton: document.querySelector('button[data-start]'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
 };
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  changeColor();
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function changeColor() {
+  intervalId = setInterval(() => {
+    const getColor = getRandomHexColor();
+    document.body.style.backgroundColor = `${getColor}`;
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearTimeout(intervalId);
+  refs.startBtn.removeAttribute('disabled');
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
