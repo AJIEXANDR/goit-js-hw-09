fdade4ed25012fce0bea4099395834c473295ee0
AJIEXANDR <104685969+AJIEXANDR@users.noreply.github.com>
2023-03-31 18:31:45 +0000

    Deploying to gh-pages from @ AJIEXANDR/goit-js-hw-09@25c7155b6ee672090c6cdc452b8eea5211430489 🚀

---
+++ 01-color-switcher.2c85d5f9.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]");
+//# sourceMappingURL=01-color-switcher.2c85d5f9.js.map
---
+++ 01-color-switcher.320a1f3c.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]");
+//# sourceMappingURL=01-color-switcher.320a1f3c.js.map
