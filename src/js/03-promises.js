7fb698dbc10d68ad2c5e547394f2ead027959092
Aleksandr Tkachenko <saylass@ukr.net>
2023-04-04 21:42:53 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', createPromiseBtn);
+
+function createPromiseBtn(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  createPromise(amount.value, delay.value)
+    .then(resolve => {
+      Notiflix.Notify.success(resolve);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error);
+    });
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
